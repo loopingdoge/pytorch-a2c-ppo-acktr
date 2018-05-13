8960f83b2f4f37ab8ec78295514a14cbd3c27ed9
Alberto Nicoletti <albyx.n@gmail.com>
2018-05-13 20:22:18 +0200

    Averaging the weights from multiple agents

--- agents/agents.py
+++ agents/agents.py
@@ -96,93 +96,69 @@ class Agents:
         if self.args.num_stack > 1:
             self.current_obs[:, :-shape_dim0] = self.current_obs[:, shape_dim0:]
         self.current_obs[:, -shape_dim0:] = obs
-    
-    def train(self):
-        print("#######")
-        print("WARNING: All rewards are clipped or normalized so you need to use a monitor (see self.envs.py) or visdom plot to get true rewards")
-        print("#######")
+
+    def init_training(self):
+        # print("#######")
+        # print("WARNING: All rewards are clipped or normalized so you need to use a monitor (see self.envs.py) or visdom plot to get true rewards")
+        # print("#######")
 
         obs = self.envs.reset()
         self.update_current_obs(obs)
         self.rollouts.observations[0].copy_(self.current_obs)
 
         # These variables are used to compute average rewards for all processes.
-        episode_rewards = torch.zeros([self.args.num_processes, 1])
-        final_rewards = torch.zeros([self.args.num_processes, 1])
+        self.episode_rewards = torch.zeros([self.args.num_processes, 1])
+        self.final_rewards = torch.zeros([self.args.num_processes, 1])
 
         if self.args.cuda:
             self.current_obs = self.current_obs.cuda()
             self.rollouts.cuda()
 
-        start = time.time()
-
-        num_updates = int(self.args.num_frames) // self.args.num_steps // self.args.num_processes
-
-        for j in range(num_updates):
-            for step in range(self.args.num_steps):
-                # Sample actions
-                with torch.no_grad():
-                    value, action, action_log_prob, states = self.actor_critic.act(
-                            self.rollouts.observations[step],
-                            self.rollouts.states[step],
-                            self.rollouts.masks[step])
-                cpu_actions = action.data.squeeze(1).cpu().numpy()
-
-                # Obser reward and next obs
-                obs, reward, done, info = self.envs.step(cpu_actions)
-                reward = torch.from_numpy(np.expand_dims(np.stack(reward), 1)).float()
-                episode_rewards += reward
-
-                # If done then clean the history of observations.
-                masks = torch.FloatTensor([[0.0] if done_ else [1.0] for done_ in done])
-                final_rewards *= masks
-                final_rewards += (1 - masks) * episode_rewards
-                episode_rewards *= masks
-
-                if self.args.cuda:
-                    masks = masks.cuda()
-
-                if self.current_obs.dim() == 4:
-                    self.current_obs *= masks.unsqueeze(2).unsqueeze(2)
-                else:
-                    self.current_obs *= masks
-
-                self.update_current_obs(obs)
-                self.rollouts.insert(self.current_obs, states.data, action.data, action_log_prob.data, value.data, reward, masks)
-
+    def compute_steps(self):
+        for step in range(self.args.num_steps):
+            # Sample actions
             with torch.no_grad():
-                next_value = self.actor_critic.get_value(self.rollouts.observations[-1],
-                                                    self.rollouts.states[-1],
-                                                    self.rollouts.masks[-1]).detach()
+                value, action, action_log_prob, states = self.actor_critic.act(
+                        self.rollouts.observations[step],
+                        self.rollouts.states[step],
+                        self.rollouts.masks[step])
+            cpu_actions = action.data.squeeze(1).cpu().numpy()
 
-            self.rollouts.compute_returns(next_value, self.args.use_gae, self.args.gamma, self.args.tau)
+            # Obser reward and next obs
+            obs, reward, done, info = self.envs.step(cpu_actions)
+            reward = torch.from_numpy(np.expand_dims(np.stack(reward), 1)).float()
+            self.episode_rewards += reward
 
-            value_loss, action_loss, dist_entropy = self.agent.update(self.rollouts)
-            
-            self.rollouts.after_update()
+            # If done then clean the history of observations.
+            masks = torch.FloatTensor([[0.0] if done_ else [1.0] for done_ in done])
+            self.final_rewards *= masks
+            self.final_rewards += (1 - masks) * self.episode_rewards
+            self.episode_rewards *= masks
 
-            if j % self.args.save_interval == 0 and self.args.save_dir != "":
-                self.save_model()
+            if self.args.cuda:
+                masks = masks.cuda()
 
-            if j % self.args.log_interval == 0:
-                end = time.time()
-                total_num_steps = (j + 1) * self.args.num_processes * self.args.num_steps
-                print("Updates {}, num timesteps {}, FPS {}, mean/median reward {:.1f}/{:.1f}, min/max reward {:.1f}/{:.1f}, entropy {:.5f}, value loss {:.5f}, policy loss {:.5f}".
-                    format(j, total_num_steps,
-                        int(total_num_steps / (end - start)),
-                        final_rewards.mean(),
-                        final_rewards.median(),
-                        final_rewards.min(),
-                        final_rewards.max(), dist_entropy,
-                        value_loss, action_loss))
-            if self.args.vis and j % self.args.vis_interval == 0:
-                try:
-                    # Sometimes monitor doesn't properly flush the outputs
-                    self.win = visdom_plot(self.viz, self.win, self.args.log_dir, self.args.env_name,
-                                    self.args.algo, self.args.num_frames)
-                except IOError:
-                    pass
+            if self.current_obs.dim() == 4:
+                self.current_obs *= masks.unsqueeze(2).unsqueeze(2)
+            else:
+                self.current_obs *= masks
 
+            self.update_current_obs(obs)
+            self.rollouts.insert(self.current_obs, states.data, action.data, action_log_prob.data, value.data, reward, masks)
+
+        with torch.no_grad():
+            next_value = self.actor_critic.get_value(self.rollouts.observations[-1],
+                                                self.rollouts.states[-1],
+                                                self.rollouts.masks[-1]).detach()
+
+        self.rollouts.compute_returns(next_value, self.args.use_gae, self.args.gamma, self.args.tau)
+
+        self.value_loss, self.action_loss, self.dist_entropy = self.agent.update(self.rollouts)
+        
+        self.rollouts.after_update()
+
+        return self.actor_critic.named_parameters()
+    
     def save_model(self):
         save_path = os.path.join(self.args.save_dir, self.args.algo)
         try:
@@ -199,3 +175,26 @@ class Agents:
                         hasattr(self.envs, 'ob_rms') and self.envs.ob_rms or None]
 
         torch.save(save_model, os.path.join(save_path, self.args.env_name + ".pt"))
+    
+    def print_progress(self, start, curr):
+        end = time.time()
+        total_num_steps = (curr + 1) * self.args.num_processes * self.args.num_steps
+        print("Updates {}, num timesteps {}, FPS {}, mean/median reward {:.1f}/{:.1f}, min/max reward {:.1f}/{:.1f}, entropy {:.5f}, value loss {:.5f}, policy loss {:.5f}".
+            format(curr, total_num_steps,
+                int(total_num_steps / (end - start)),
+                self.final_rewards.mean(),
+                self.final_rewards.median(),
+                self.final_rewards.min(),
+                self.final_rewards.max(), self.dist_entropy,
+                self.value_loss, self.action_loss))
+    
+    def plot(self):
+        try:
+            # Sometimes monitor doesn't properly flush the outputs
+            self.win = visdom_plot(self.viz, self.win, self.args.log_dir, self.args.env_name,
+                            self.args.algo, self.args.num_frames)
+        except IOError:
+            pass
+    
+    def substitute_params(self, params):
+        self.actor_critic.load_state_dict(params)
--- main.py
+++ main.py
@@ -1,10 +1,46 @@
+import time
+
 from agents import Agents
 from agents.arguments import get_args
+from copy import deepcopy
 
-def main():
+
+def multiple_agents(n_agents):
     args = get_args()
-    acktr = Agents(args)
-    acktr.train()
+
+    num_updates = int(args.num_frames) // args.num_steps // args.num_processes
+
+    agents = [Agents(args) for i in range(n_agents)]
+
+    for a in agents:
+        a.init_training()
+    
+    start = time.time()
+
+    for j in range(num_updates):
+        params = list(map(lambda a: dict(a.compute_steps()), agents))
+
+        params_sum = deepcopy(params[0])
+        for p in params[1:]:
+            for name, param in p.items():
+                params_sum[name].data.copy_(params_sum[name].data + param.data)
+
+        params_average = deepcopy(params_sum)
+        for name, param in params_sum.items():
+            params_average[name].data.copy_(params_sum[name].data * 0.5)
+
+        for a in agents:
+            a.substitute_params(params_average)
+
+        if j % args.save_interval == 0 and args.save_dir != "":
+            agents[0].save_model()
+
+            if j % args.log_interval == 0:
+                agents[0].print_progress(start, j)
+
+            if args.vis and j % args.vis_interval == 0:
+                agents[0].plot()
+
 
 if __name__ == "__main__":
-    main()
+    multiple_agents(3)
