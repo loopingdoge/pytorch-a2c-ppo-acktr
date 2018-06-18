dd1add904eb912dead5490bd6c537687d58c70bf
Alberto Nicoletti <albyx.n@gmail.com>
2018-06-18 16:20:03 +0200

    Save and load weights, not working yet but it runs

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from agents import Agents
 from agents.arguments import get_args
 
+
 def main():
     args = get_args()
     acktr = Agents(args)
     acktr.train()
+
 
 if __name__ == "__main__":
     main()
